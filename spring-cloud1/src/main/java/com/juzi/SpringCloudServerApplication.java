6789d499478bed6052f5ecdb3de93ac6e742fa23
yangzhiw <yang993505105@gamil.com>
2016-09-08 14:29:59 +0800

    增加 监控

---
+++ spring-cloud1/src/main/java/com/juzi/SpringCloudServerApplication.java
@@ -0,0 +1,12 @@
+package com.juzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServerApplication.class, args);
+	}
+}
